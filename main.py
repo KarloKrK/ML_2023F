d0ef6134e3826efd5e40189cb4e8c48537d30273
KarloKrK <145758877+KarloKrK@users.noreply.github.com>
2023-09-22 13:33:21 +0300

    Update main.py
    Iterating project further

--- main.py
+++ main.py
@@ -4,7 +4,24 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split  # Import train_test_split
 import matplotlib.pyplot as plt
 
-# todo overall: visualise data.
+# todo
+#      1. Code organization
+#           Reduce repetition
+#           encapsulate manual labor and repeated logic into functions
+#      2. Data exploration
+#           data imputation
+#           visualisation
+#      3. Feature Engineering
+#           Correlation analysis
+#      4. Model evaluation
+#           cross validation
+#           feature importance
+#           multiple models
+#           hyperparameter tuning
+#      5. Reporting
+#           interpretability
+#           logging
+
 # Load data from the specified directory
 input_dir = "C:/Users/Karlo/PycharmProjects/ML_project/input"
 
@@ -61,24 +78,7 @@ X = happiness.drop("Score", axis=1)
 y = happiness["Score"]
 
 
-# Create a 1x5 grid for 5 subplots
-fig, axes = plt.subplots(1, 4, figsize=(20, 4))
 
-# Plot the scatter plots
-axes[0].scatter(X.Economy, y)
-axes[0].set_title("Economy")
-
-axes[1].scatter(X.Health, y)
-axes[1].set_title("Health")
-
-axes[2].scatter(X.Freedom, y)
-axes[2].set_title("Freedom")
-
-axes[3].scatter(X.Generosity, y)
-axes[3].set_title("Generosity")
-
-# Show the plots
-plt.show()
 
 # Split the data into training, testing, and validation sets
 X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
@@ -105,3 +105,57 @@ mse_val = mean_squared_error(y_val, y_pred_val)
 # Print the MSE values for testing and validation
 print("Mean Squared Error (MSE) on Testing Set:", mse_test)
 print("Mean Squared Error (MSE) on Validation Set:", mse_val)
+
+
+# Visualisation
+#     Some of these might not function properly
+import seaborn as sns
+
+correlation_matrix = happiness.corr()
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+plt.title("Correlation Heatmap")
+plt.show()
+
+# Create a 1x5 grid for 5 subplots
+fig, axes = plt.subplots(1, 4, figsize=(20, 4))
+
+# Plot the scatter plots
+axes[0].scatter(X.Economy, y)
+axes[0].set_title("Economy")
+
+axes[1].scatter(X.Health, y)
+axes[1].set_title("Health")
+
+axes[2].scatter(X.Freedom, y)
+axes[2].set_title("Freedom")
+
+axes[3].scatter(X.Generosity, y)
+axes[3].set_title("Generosity")
+
+# Show the plots
+plt.show()
+
+sns.pairplot(happiness)
+plt.show()
+
+import numpy as np
+
+residuals = y_test - y_pred_test
+plt.scatter(y_pred_test, residuals)
+plt.hlines(y=0, xmin=np.min(y_pred_test), xmax=np.max(y_pred_test), colors='r')
+plt.xlabel("Predicted")
+plt.ylabel("Residuals")
+plt.title("Residual Plot")
+plt.show()
+
+features = X.columns
+coefficients = lm.coef_
+feature_importance = pd.Series(coefficients, index=features).sort_values()
+
+feature_importance.plot(kind='barh')
+plt.title("Feature Importance")
+plt.show()
+
+sns.boxplot(x='Economy', y='Score', data=happiness)
+plt.title("Economy vs. Score")
+plt.show()
